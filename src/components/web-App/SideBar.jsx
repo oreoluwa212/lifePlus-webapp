60853503948c5acfa2d0cd6fb5dcdfa0664fb1eb
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2024-02-09 08:21:44 +0100

    sidebar content added

--- src/components/web-App/SideBar.jsx
+++ src/components/web-App/SideBar.jsx
@@ -4,15 +4,15 @@ import Navlinks from "./NavLinks";
 
 const SideBar = () => {
   return (
-    <div className="lgss:w-1/5 bg-[#F0EDE7] lgss:h-screen lgss:overflow-hidden border-gold border-[1px] flex flex-col gap-5 px-6 pt-8">
-      <div className="flex lgss:justify-center lgss:items-center ">
+    <div className="lgss:w-1/5 bg-[#F0EDE7] lgss:h-screen lgss:overflow-hidden h-screen border-gold border-[1px] flex flex-col gap-5 px-6 pt-8">
+      <div className="flex justify-center items-center ">
         <Link to={"/"}>
           <h1 className="text-black text-[36px] font-semibold ">
             Life<span className="text-red">Plus</span>
           </h1>
         </Link>
       </div>
-      <div className=" lgss:w-full w-fit hidden lgss:flex lgss:justify-center lgss:flex-col lgss:items-start px-6">
+      <div className=" lgss:w-full lgss:flex lgss:justify-center lgss:flex-col lgss:items-start px-6">
         <div className="w-1/5 lgss:h-screen ">
           <div className="">
             <div className="lgss:mt-12">
