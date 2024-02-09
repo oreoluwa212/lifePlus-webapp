b5a5b3be6132eab8270f1a8ddb2bda34be40f5d7
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-02-09 18:55:30 +0100

    Details Card added to dashboards

--- src/components/web-App/DashboardCard.jsx
+++ src/components/web-App/DashboardCard.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const DashboardCard = () => {
-  return <div className="bg-[#F3E4E4] lgss:h-[300px] lgss:mt-9 rounded-[32px] lgss:flex justify-center items-center lgss:w-[300px]">DashboardCard</div>;
+  return (
+    <div className="bg-[#F3E4E4] lgss:h-[300px] lgss:mt-9 rounded-[32px] lgss:flex justify-center items-center hidden lgss:w-[300px]">
+      DashboardCard
+    </div>
+  );
 }
 
 export default DashboardCard
--- src/components/web-App/DetailsCard.jsx
+++ src/components/web-App/DetailsCard.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 
 const DetailsCard = () => {
   return (
-    <div className="lgss:w-[70%] bg-opacity-90 bg-[#dda195] bg-gradient-to-br from-[#e7b6ad] via-[#c77261] to-[#D2897A] h-[20%] lgss:mt-6 rounded-[32px] lgss:flex lgss:items-center px-6">
-      <div className="lgss:flex lgss:justify-between w-full lgss:px-6">
+    <div className="lgss:w-[70%] hidden bg-opacity-90 bg-[#dda195] bg-gradient-to-br from-[#e7b6ad] via-[#c77261] to-[#D2897A] h-[20%] lgss:mt-6 rounded-[32px] lgss:flex lgss:items-center px-6">
+      <div className="  lgss:flex lgss:justify-between w-full lgss:px-6">
         <div className="lgss:flex lgss:flex-col items-center">
           <p className="text-[20px]">Gender</p>
-          <h1 className="text-[40px] font-semibold">M</h1>
+          <h1 className="lgss:text-[40px] font-semibold">M</h1>
         </div>
         <div className="lgss:flex lgss:flex-col items-center">
           <p className="text-[20px]">Weight</p>
-          <h1 className="text-[40px] font-semibold">68kg</h1>
+          <h1 className="lgss:text-[40px] font-semibold">68kg</h1>
         </div>
         <div className="lgss:flex lgss:flex-col items-center">
           <p className="text-[20px]">Blood Type</p>
-          <h1 className="text-[40px] font-semibold">O-</h1>
+          <h1 className="lgss:text-[40px] font-semibold">O-</h1>
         </div>
         <div className="lgss:flex lgss:flex-col items-center">
           <p className="text-[20px]">Units Donated</p>
-          <h1 className="text-[40px] font-bold">5</h1>
+          <h1 className="lgss:text-[40px] font-bold">5</h1>
         </div>
       </div>
     </div>
