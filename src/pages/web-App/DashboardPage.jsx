81f18527a52b33ad3f1a38aed34ddc2295bf03a6
Adetunji Adebowale <bowaleadetunji@gmail.com>
2024-02-21 07:45:12 +0100

    added google Oauth

--- src/pages/web-App/DashboardPage.jsx
+++ src/pages/web-App/DashboardPage.jsx
@@ -13,6 +13,7 @@ import { useLocation, useNavigate, Link } from "react-router-dom";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import { useEffect } from "react";
+import axios from "axios";
 
 const DashboardPage = () => {
   const navigate = useNavigate();
@@ -24,7 +25,7 @@ const DashboardPage = () => {
     newUser = location.state.user;
   }
 
-  const loggedInUser = JSON.parse(localStorage.getItem("user"));
+  let loggedInUser = JSON.parse(localStorage.getItem("user"));
   if (newUser && newUser.new) {
     useEffect(() => {
       MySwal.fire({
@@ -36,7 +37,34 @@ const DashboardPage = () => {
       navigate("/update-profile", { state: { user: newUser } });
     }, []);
   } else {
-    if (!loggedInUser) {
+    // check for googleOauth loggedin User
+    let userId = "";
+    if (location.search) {
+      userId = location.search.slice(1);
+      useEffect(() => {
+        const fetchgoogleUser = async () => {
+          try {
+            let response = await axios.get(
+              `https://lifeplus-api.onrender.com/user?userid=${userId}`
+            );
+
+            MySwal.fire({
+              icon: "info",
+              title: "Profile Update Required",
+              text: "hi, in other for us to know you better and personalize your experience, you are expected to update your profile",
+              footer: '<a href="/update-profile">Update Now</a>',
+            });
+            // location.search
+            loggedInUser = response.data;
+            localStorage.setItem("user", JSON.stringify(response.data));
+            navigate("/update-profile", { state: { user: response.data } });
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchgoogleUser();
+      }, []);
+    } else if (!loggedInUser) {
       useEffect(() => {
         MySwal.fire("you dont have access to view this page");
         navigate("/login");
@@ -45,10 +73,16 @@ const DashboardPage = () => {
   }
   return (
     <div className="flex flex-row h-screen w-full">
-      <SideBar isOpen={isOpen}/>
+      <SideBar isOpen={isOpen} />
       <div className="lgss:w-4/5 w-full lgss:overflow-auto">
         <div className="lgss:w-full lgss:h-full lgss:flex-1 lgss:overflow lgss:px-[4%] flex flex-col ">
-          {loggedInUser && <Header setIsOpen={setIsOpen} isOpen={isOpen} currentUser={loggedInUser} />}{" "}
+          {loggedInUser && (
+            <Header
+              setIsOpen={setIsOpen}
+              isOpen={isOpen}
+              currentUser={loggedInUser}
+            />
+          )}{" "}
           <div className="mt-[50px] lgss:mt-0 text-[34px] text-gold font-semibold px-[5%] mds:px-0">
             <h1>Dashboard</h1>
           </div>
@@ -56,11 +90,11 @@ const DashboardPage = () => {
           <div className="">
             <div className="flex flex-col justify-center lgss:items-start lgss:pt-14 pt-4">
               <div className=" flex flex-col lgss:grid lgss:grid-cols-3 gap-8">
-                <Link to={'/dashboard/profile'}>
-                <DashboardCard
-                  title="Manage Profile"
-                  icon={<PiUserCircleFill />}
-                />
+                <Link to={"/dashboard/profile"}>
+                  <DashboardCard
+                    title="Manage Profile"
+                    icon={<PiUserCircleFill />}
+                  />
                 </Link>
 
                 <DashboardCard
@@ -71,12 +105,12 @@ const DashboardPage = () => {
                   title="News & Updates"
                   icon={<PiArticleFill />}
                 />
-                <Link to={'/dashboard/points'}>
-                <DashboardCard
-                  title="Points & Badges"
-                  icon={<PiShieldCheckFill />}
-                />
-                 </Link> 
+                <Link to={"/dashboard/points"}>
+                  <DashboardCard
+                    title="Points & Badges"
+                    icon={<PiShieldCheckFill />}
+                  />
+                </Link>
               </div>
             </div>
           </div>
