3da95f18d503639a3cef52cd6a70fcfef0b15c0e
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-02-22 08:26:45 +0100

    Paasword eye icon

--- src/pages/SignUpMainPage.jsx
+++ src/pages/SignUpMainPage.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 
 const SignUpMainPage = () => {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
@@ -66,10 +67,10 @@ const SignUpMainPage = () => {
   return (
     <div className="">
       <NavBar />
-      <div className="mds:flex relative mds:absolute flex mds:flex-row h-[83vh] mds:overflow-y-hidden border-t-2 border-red w-full">
+      <div className="mds:flex relative mds:absolute flex flex-row mds:overflow-y-hidden border-t-2 border-red w-full">
         <Demo />
-        <div className=" mds:w-3/5 mx-[5%] mds:mx-0 bg-white rounded-[32px] mds:bg-transparent mt-[10%] mds:mt-[2%] mds:relative h-[75%] mds:h-screen overflow-auto flex flex-col items-center absolute top-0 left-0 ">
-          <div className="lgss:w-full px-[5%] mds:px-0 lgss:flex flex-col lgss:justify-center lgss:items-center lgss:gap-3 ">
+        <div className=" mds:w-3/5 bg-white rounded-[32px] mds:bg-transparent  mt-[10%] mds:mt-0 py-[2%] mds:relative h-[80%] mds:h-full overflow-auto flex flex-col justify-center items-center absolute top-0 left-0 ">
+          <div className="lgss:w-full px-[5%] mds:px-0 lgss:flex flex-col lgss:justify-center lgss:items-center lgss:gap-16 ">
             <div className="lgss:flex lgss:flex-col lgss:items-center ">
               <h1 className="text-gold font-bold text-[30px]">
                 Sign Up for LifePlus
@@ -108,64 +109,33 @@ const SignUpMainPage = () => {
               {emailError && (
                 <small className="text-red">email is required</small>
               )}
-              <div className="bg-transparent border-gold border-2 rounded-[32px] lgss:pl-8 lgss:pr-2 lgss:h-[48px] h-[50px] pl-4 mds:px-0  flex outline-none  text-[16px] w-full justify-between">
+              <div className=" border-gold bg-white lgss:bg-transparent border-2 rounded-[32px] lgss:pl-8 lgss:pr-2 lgss:h-[48px] h-[50px] pl-4 mds:px-0  flex outline-none  text-[16px] w-full justify-between">
                 <input
                   type={isPasswordVisible ? "text" : "password"}
                   placeholder="password"
-                  className=" lgss:h-[48px] outline-none  text-[16px] bg-transparent"
+                  className=" lgss:h-[48px] outline-none bg-transparent text-[16px]"
                   onChange={(e) => {
                     setPassword(e.target.value);
                   }}
                 />
-                <button
-                  className=" px-4 text-gold"
-                  onClick={togglePasswordVisibility}
-                >
-                  {isPasswordVisible ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                      />
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                      />
-                    </svg>
-                  )}
-                </button>
-              </div>
 
+                {isPasswordVisible ? (
+                  <IoMdEye
+                    onClick={togglePasswordVisibility}
+                    className="cursor-pointer text-xl relative top-4 text-gold right-4"
+                  />
+                ) : (
+                  <IoMdEyeOff
+                    onClick={togglePasswordVisibility}
+                    className="cursor-pointer text-xl relative top-4 text-gold right-4"
+                  />
+                )}
+              </div>
               {passwordError && (
                 <small className="text-red">password is required</small>
               )}
-
               <div className="flex px-4 gap-4 mt-2">
-                <input type="checkbox" name="aggree" id="aggree" required/>
+                <input type="checkbox" name="aggree" id="aggree" required />
                 <p className="text-black">
                   I agree to LifePlus Terms and conditions and the privacy
                   policy
