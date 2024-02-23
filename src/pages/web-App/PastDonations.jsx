cefa8083357fe4217d47a22544de9551e46cf0e2
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-02-23 01:04:15 +0100

    feat: Time for schedule  added

--- src/pages/web-App/PastDonations.jsx
+++ src/pages/web-App/PastDonations.jsx
@@ -1,31 +1,33 @@
 import React from "react";
-import Header from "../../components/web-App/Header";
 import SideBar from "../../components/web-App/SideBar";
-import { FaAngleRight } from "react-icons/fa";
+import ForumHeader from "../../components/web-App/ForumHeader";
 
 const PastDonations = () => {
+  const loggedInUser = JSON.parse(localStorage.getItem("user"));
+  
   return (
     <div className="flex flex-row h-screen">
       <SideBar />
-      <div className="lgss:w-4/5 lgss:overflow-auto">
-        <div className="lgss:w-full lgss:h-full lgss:flex-1 lgss:overflow lgss:px-[4%] flex flex-col ">
-          <Header />
-          <div className="mt-[50px] text-[34px] text-gold font-semibold px-4 lgss:px-0">
-            <h1 className="lgss:flex items-center gap-2">
-              Donations{" "}
-              <span className="text-[20px] font-light">
-                <FaAngleRight />
-              </span>
-              <p className="text-[20px] font-normal">Donations History</p>
-            </h1>
+      <div className=" lgss:w-4/5 w-full h-full overflow-auto lgss:px-[4%] flex flex-col lgss:gap-12">
+        <ForumHeader currentUser={loggedInUser} />
+        <div className="flex gap-9 mt-10 lgss:mt-4 px-[5%] mds:px-0 w-full">
+          <div className="flex flex-col justify-between lgss:gap-5 lgss:items-center ">
             <div className="">
-              
+              <p>
+                Collect your favorite rewards in one place. Start redeeming your
+                rewards.
+              </p>
             </div>
+            <button
+              type="submit"
+              className="h-[48px] lgss:mt-6 mt-4 mds:mt-0 w-[100%] mds:w-[70%] flex justify-center items-center bg-red text-white rounded-[32px] text-[1.2rem]"
+            >
+              Redeem Reward
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default PastDonations;
