0d2b8bee0816363b007f667e958986b48f81ec4b
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-02-23 01:00:55 +0100

    feat: Time for schedule  added

--- src/pages/web-App/AppointmentsScheduleDonor.jsx
+++ src/pages/web-App/AppointmentsScheduleDonor.jsx
@@ -54,8 +54,15 @@ const AppointmentsScheduleDonor = () => {
           <div className="mt-[50px] lgss:mt-0 text-[34px] text-gold font-semibold px-[5%] mds:px-0 ">
             <h1>Appointments | Schedule</h1>
           </div>
-          <SubmitSchheduleDonor isOpen={isOpen} handleClose={handleCloseModal} />
-          <form onSubmit={handleFormSubmit} onKeyPress={handleFormKeyPress} className="flex flex-col px-[4%] lgss:px-0">
+          <SubmitSchheduleDonor
+            isOpen={isOpen}
+            handleClose={handleCloseModal}
+          />
+          <form
+            onSubmit={handleFormSubmit}
+            onKeyPress={handleFormKeyPress}
+            className="flex flex-col px-[4%] lgss:px-0"
+          >
             <div className="relative flex justify-between items-center bg-[#F3E4E4] mt-4 w-[80%] lgss:w-[400px] h-[50px] px-6 rounded-[32px]">
               <DatePicker
                 selected={selectedDate}
@@ -67,13 +74,16 @@ const AppointmentsScheduleDonor = () => {
                 className="w-full h-full bg-[#F3E4E4] outline-none placeholder:text-gold placeholder:font-semibold placeholder:text-[18px]"
                 open={calendarOpen}
               />
-              <FaAngleDown className="cursor-pointer " onClick={toggleCalendar} />
+              <FaAngleDown
+                className="cursor-pointer "
+                onClick={toggleCalendar}
+              />
             </div>
             <div className="mt-4 flex flex-col">
               <p className="font-semibold">Select a Time</p>
               <div className="flex gap-6 mt-4 text-[18px]">
                 <button
-                type="button"
+                  type="button"
                   className={`h-[60px] w-[90px] rounded-[16px] ${
                     selectedTime === "9:00am" ? "bg-pink-200" : "bg-white"
                   } shadow-lg shadow-gray-200/50 border border-gray-300`}
@@ -82,7 +92,7 @@ const AppointmentsScheduleDonor = () => {
                   9:00am
                 </button>
                 <button
-                type="button"
+                  type="button"
                   className={`h-[60px] w-[90px] rounded-[16px] ${
                     selectedTime === "10:00am" ? "bg-pink-200" : "bg-white"
                   } shadow-lg shadow-gray-200/50 border border-gray-300`}
@@ -90,7 +100,42 @@ const AppointmentsScheduleDonor = () => {
                 >
                   10:00am
                 </button>
-                {/* Repeat for other buttons, updating the time value */}
+                <button
+                  type="button"
+                  className={`h-[60px] w-[90px] rounded-[16px] ${
+                    selectedTime === "11:00am" ? "bg-pink-200" : "bg-white"
+                  } shadow-lg shadow-gray-200/50 border border-gray-300`}
+                  onClick={() => setSelectedTime("11:00am")}
+                >
+                  11:00am
+                </button>
+                <button
+                  type="button"
+                  className={`h-[60px] w-[90px] rounded-[16px] ${
+                    selectedTime === "12:00pm" ? "bg-pink-200" : "bg-white"
+                  } shadow-lg shadow-gray-200/50 border border-gray-300`}
+                  onClick={() => setSelectedTime("12:00pm")}
+                >
+                  12:00pm
+                </button>
+                <button
+                  type="button"
+                  className={`h-[60px] w-[90px] rounded-[16px] ${
+                    selectedTime === "01:00pm" ? "bg-pink-200" : "bg-white"
+                  } shadow-lg shadow-gray-200/50 border border-gray-300`}
+                  onClick={() => setSelectedTime("10:00pm")}
+                >
+                  01:00pm
+                </button>
+                <button
+                  type="button"
+                  className={`h-[60px] w-[90px] rounded-[16px] ${
+                    selectedTime === "02:00pm" ? "bg-pink-200" : "bg-white"
+                  } shadow-lg shadow-gray-200/50 border border-gray-300`}
+                  onClick={() => setSelectedTime("02:00pm")}
+                >
+                  02:00pm
+                </button>
               </div>
               <button
                 type="submit"
