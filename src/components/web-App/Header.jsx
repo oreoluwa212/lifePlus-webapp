799efbecec93b4a603c855fd672a5baf87a4a695
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-02-23 01:25:31 +0100

    feat: Donation history initialized

--- src/components/web-App/Header.jsx
+++ src/components/web-App/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { user } from "../../assets";
 import {
-  PiMagnifyingGlassBold,
   PiMagnifyingGlassFill,
   PiMedalBold,
 } from "react-icons/pi";
--- src/pages/web-App/PastDonations.jsx
+++ src/pages/web-App/PastDonations.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import SideBar from "../../components/web-App/SideBar";
 import ForumHeader from "../../components/web-App/ForumHeader";
 import { Link } from "react-router-dom";
-import { PiArrowLeftBold } from "react-icons/pi";
+import { PiArrowLeftBold, PiMagnifyingGlassFill } from "react-icons/pi";
 
 const PastDonations = () => {
   const loggedInUser = JSON.parse(localStorage.getItem("user"));
@@ -19,12 +19,18 @@ const PastDonations = () => {
         </div>
         <div className="flex gap-9 mt-10 lgss:mt-4 px-[5%] mds:px-0 w-full">
           <div className="flex flex-col justify-between lgss:gap-5 lgss:items-center ">
-            <div className="">
+              <div className="bg-[#F3E4E4] w-[50%] lgss:w-[65%] lgss:h-[80%] flex justify-between items-center px-4 rounded-[32px]">
+                <PiMagnifyingGlassFill className="text-gold text-[35px] mds:text-[24px] cursor-pointer" />
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="w-[80%] h-[48px] px-2 outline-none placeholder:text-gold placeholder:font-semibold placeholder:text-[18px] bg-transparent"
+                />
+              </div>
               <p>
                 Collect your favorite rewards in one place. Start redeeming your
                 rewards.
               </p>
-            </div>
             <button
               type="submit"
               className="h-[48px] lgss:mt-6 mt-4 mds:mt-0 w-[100%] mds:w-[70%] flex justify-center items-center bg-red text-white rounded-[32px] text-[1.2rem]"
