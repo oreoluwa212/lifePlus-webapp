c8554cefbde7ba74197fc454b27f077721902fbd
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2024-02-25 13:34:38 +0100

    fix: login/signup pages styling

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -9,7 +9,7 @@ const SignUpPage = () => {
       <NavBar />
       <div className="relative mds:absolute mds:flex mds:flex-row h-[100vh] border-t-2 border-red w-full">
         <Demo />
-        <div className=" mds:w-3/5 w-full absolute mds:relative top-0 mt-[5%] mds:mt-0 mds:h-full overflow-auto flex flex-col justify-end mds:justify-center pb-[40px] items-center h-[70vh]">
+        <div className=" mds:w-3/5 w-full absolute mds:relative top-0 mt-[5%] mds:mt-0 mds:h-full overflow-hidden flex flex-col justify-end mds:justify-center pb-[40px] items-center h-[70vh]">
           <div className="w-full flex flex-col justify-center items-center mds:gap-12 xs:gap-8">
             <div className="flex flex-col mds:gap-6 items-center">
               <h1 className="text-gold font-bold text-[30px]">
