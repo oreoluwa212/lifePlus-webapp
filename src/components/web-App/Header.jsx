5e6859657f74b8dc5c44b7ad3a914330bc4b1fcc
Adetunji Adebowale <bowaleadetunji@gmail.com>
2024-03-03 00:17:57 +0100

    fix live link breakage bug, google signin not wprking yet

--- src/components/web-App/Header.jsx
+++ src/components/web-App/Header.jsx
@@ -1,89 +1,89 @@
-// import React, { useState } from "react";
-// import { user } from "../../assets";
-// import { PiMagnifyingGlassFill, PiMedalBold } from "react-icons/pi";
-// import { FaBars, FaTimes } from "react-icons/fa";
-// import { Link } from "react-router-dom";
-// import DropdownModal from "./DropdownModal";
-// import generateBlob from "./generateBlob";
+import React, { useState } from "react";
+import { user } from "../../assets";
+import { PiMagnifyingGlassFill, PiMedalBold } from "react-icons/pi";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import DropdownModal from "./DropdownModal";
+import generateBlob from "./generateBlob";
 
-// const Header = ({ currentUser, setIsOpen, isOpen }) => {
-//   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+const Header = ({ currentUser, setIsOpen, isOpen }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-//   const toggleDropdown = () => {
-//     setIsDropdownOpen(!isDropdownOpen);
-//   };
-//   const handleLogout = () => {
-//     localStorage.clear();
-//   };
-//   const avatar = generateBlob(currentUser);
-//   return (
-//     <div className="">
-//       <div className="w-full h-[100px] hidden lgss:flex lgss:justify-between items-center lgss:pt-8 lgss:mb-10">
-//         <div className="bg-[#F3E4E4] w-[50%] lgss:w-[65%] lgss:h-[80%] flex justify-between items-center px-4 rounded-[32px]">
-//           <PiMagnifyingGlassFill className="text-gold text-[35px] mds:text-[24px] cursor-pointer" />
-//           <input
-//             type="text"
-//             placeholder="Search"
-//             className="lgss:w-full lgss:h-full lgss:mx-2 placeholder:px-2 outline-none placeholder:text-gold placeholder:font-semibold placeholder:text-[18px] bg-transparent"
-//           />
-//         </div>
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+  const handleLogout = () => {
+    localStorage.clear();
+  };
+  const avatar = generateBlob(currentUser);
+  return (
+    <div className="">
+      <div className="w-full h-[100px] hidden lgss:flex lgss:justify-between items-center lgss:pt-8 lgss:mb-10">
+        <div className="bg-[#F3E4E4] w-[50%] lgss:w-[65%] lgss:h-[80%] flex justify-between items-center px-4 rounded-[32px]">
+          <PiMagnifyingGlassFill className="text-gold text-[35px] mds:text-[24px] cursor-pointer" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="lgss:w-full lgss:h-full lgss:mx-2 placeholder:px-2 outline-none placeholder:text-gold placeholder:font-semibold placeholder:text-[18px] bg-transparent"
+          />
+        </div>
 
-//         <div className=" w-[100%] lgss:w-[30%] px-4 h-[45%] hidden lg:flex gap-6 items-center rounded-[32px]">
-//           {currentUser.avatar ? (
-//             <img
-//               src={avatar}
-//               alt="user Avatar"
-//               className="w-[50px] rounded-[50px]"
-//             />
-//           ) : (
-//             <img src={user} alt="" className="w-[50px]" />
-//           )}
-//           <button
-//             onClick={toggleDropdown}
-//             className="flex items-start flex-col justify-between gap-1 "
-//           >
-//             <h1 className="text-[16px] mds:text-[20px] font-semibold lgss:w-[180px]">
-//               {currentUser.fullName
-//                 ? currentUser.fullName
-//                 : currentUser.facilityName}
-//             </h1>
-//             <div className="flex gap-2">
-//               <PiMedalBold className="text-gold mdss:flex text-[26px] mds:text-[22px]" />
-//               <p className="text-[18px]">Bronze member</p>
-//             </div>
-//           </button>
-//           <DropdownModal
-//             isOpen={isDropdownOpen}
-//             handleClose={() => setIsDropdownOpen(false)}
-//             handleLogout={handleLogout}
-//             currentUser={currentUser}
-//           />
-//         </div>
-//       </div>
-//       <div className="flex lgss:hidden justify-between items-center h-[100px] w-full px-8">
-//         <div className="">
-//           <Link to={"/"}>
-//             <h1 className="text-black text-[36px] font-semibold">
-//               Life<span className="text-red">Plus</span>
-//             </h1>
-//           </Link>
-//         </div>
-//         <div className="">
-//           {isOpen ? (
-//             <FaTimes
-//               onClick={() => setIsOpen(false)}
-//               className=" cursor-pointer text-red text-xl"
-//             />
-//           ) : (
-//             <FaBars
-//               onClick={() => setIsOpen(true)}
-//               className=" cursor-pointer text-red text-xl"
-//             />
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+        <div className=" w-[100%] lgss:w-[30%] px-4 h-[45%] hidden lg:flex gap-6 items-center rounded-[32px]">
+          {currentUser.avatar ? (
+            <img
+              src={avatar}
+              alt="user Avatar"
+              className="w-[50px] rounded-[50px]"
+            />
+          ) : (
+            <img src={user} alt="" className="w-[50px]" />
+          )}
+          <button
+            onClick={toggleDropdown}
+            className="flex items-start flex-col justify-between gap-1 "
+          >
+            <h1 className="text-[16px] mds:text-[20px] font-semibold lgss:w-[180px]">
+              {currentUser.fullName
+                ? currentUser.fullName
+                : currentUser.facilityName}
+            </h1>
+            <div className="flex gap-2">
+              <PiMedalBold className="text-gold mdss:flex text-[26px] mds:text-[22px]" />
+              <p className="text-[18px]">Bronze member</p>
+            </div>
+          </button>
+          <DropdownModal
+            isOpen={isDropdownOpen}
+            handleClose={() => setIsDropdownOpen(false)}
+            handleLogout={handleLogout}
+            currentUser={currentUser}
+          />
+        </div>
+      </div>
+      <div className="flex lgss:hidden justify-between items-center h-[100px] w-full px-8">
+        <div className="">
+          <Link to={"/"}>
+            <h1 className="text-black text-[36px] font-semibold">
+              Life<span className="text-red">Plus</span>
+            </h1>
+          </Link>
+        </div>
+        <div className="">
+          {isOpen ? (
+            <FaTimes
+              onClick={() => setIsOpen(false)}
+              className=" cursor-pointer text-red text-xl"
+            />
+          ) : (
+            <FaBars
+              onClick={() => setIsOpen(true)}
+              className=" cursor-pointer text-red text-xl"
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default Header;
+export default Header;
--- src/pages/web-App/DashboardPage.jsx
+++ src/pages/web-App/DashboardPage.jsx
@@ -16,117 +16,115 @@ import withReactContent from "sweetalert2-react-content";
 import { useEffect } from "react";
 import axios from "axios";
 
-console.log("ek.;lk");
-// const DashboardPage = () => {
-//   const navigate = useNavigate();
-//   const [isOpen, setIsOpen] = useState(false);
-//   const MySwal = withReactContent(Swal);
-//   let newUser = {};
-//   let location = useLocation();
-//   if (location.state) {
-//     newUser = location.state.user;
-//   }
+const DashboardPage = () => {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+  const MySwal = withReactContent(Swal);
+  let newUser = {};
+  let location = useLocation();
+  if (location.state) {
+    newUser = location.state.user;
+  }
 
-//   let loggedInUser = JSON.parse(localStorage.getItem("user"));
-//   if (newUser && newUser.new) {
-//     useEffect(() => {
-//       MySwal.fire({
-//         icon: "info",
-//         title: "Profile Update Required",
-//         text: "hi, in other for us to know you better and personalize your experience, you are expected to update your profile",
-//         footer: '<a href="/update-profile">Update Now</a>',
-//       });
-//       navigate("/update-profile", { state: { user: newUser } });
-//     }, []);
-//   } else {
-//     // check for googleOauth loggedin User
+  let loggedInUser = JSON.parse(localStorage.getItem("user"));
+  if (newUser && newUser.new) {
+    useEffect(() => {
+      MySwal.fire({
+        icon: "info",
+        title: "Profile Update Required",
+        text: "hi, in other for us to know you better and personalize your experience, you are expected to update your profile",
+        footer: '<a href="/update-profile">Update Now</a>',
+      });
+      navigate("/update-profile", { state: { user: newUser } });
+    }, []);
+  } else {
+    // check for googleOauth loggedin User
+    let userId = "";
+    if (location.search) {
+      userId = location.search.slice(1);
+      useEffect(() => {
+        const fetchgoogleUser = async () => {
+          try {
+            let response = await axios.get(
+              `https://lifeplus-api.onrender.com/user?userid=${userId}`
+            );
 
-//     let userId = "";
-//     if (location.search) {
-//       userId = location.search.slice(1);
-//       useEffect(() => {
-//         const fetchgoogleUser = async () => {
-//           try {
-//             let response = await axios.get(
-//               `https://lifeplus-api.onrender.com/user?userid=${userId}`
-//             );
+            MySwal.fire({
+              icon: "info",
+              title: "Profile Update Required",
+              text: "hi, in other for us to know you better and personalize your experience, you are expected to update your profile",
+              footer: '<a href="/update-profile">Update Now</a>',
+            });
+            // location.search
+            loggedInUser = response.data;
+            localStorage.setItem("user", JSON.stringify(response.data));
+            if (!loggedInUser.accountUpdated) {
+              navigate("/update-profile", { state: { user: response.data } });
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchgoogleUser();
+      }, []);
+    } else if (!loggedInUser) {
+      useEffect(() => {
+        MySwal.fire("you dont have access to view this page");
+        navigate("/login");
+      }, []);
+    }
+  }
+  return (
+    <div className="flex flex-row h-screen w-full">
+      <SideBar isOpen={isOpen} />
+      <div className="lgss:w-4/5 w-full lgss:overflow-auto">
+        <div className="lgss:w-full lgss:h-full lgss:flex-1 lgss:overflow lgss:px-[4%] flex flex-col ">
+          {loggedInUser && (
+            <Header
+              setIsOpen={setIsOpen}
+              isOpen={isOpen}
+              currentUser={loggedInUser}
+            />
+          )}{" "}
+          <div className="mt-[50px] lgss:mt-0 text-[34px] text-gold font-semibold px-[5%] mds:px-0">
+            <h1>Dashboard</h1>
+          </div>
+          {loggedInUser && <DetailsCard loggedInUser={loggedInUser} />}
+          <div className="">
+            <div className="flex flex-col justify-center lgss:items-start lgss:pt-14 pt-4">
+              <div className=" flex flex-col lgss:grid lgss:grid-cols-3 gap-8">
+                <Link to={"/dashboard/profile"}>
+                  <DashboardCard
+                    title="Manage Profile"
+                    icon={<PiUserCircleFill />}
+                  />
+                </Link>
+                <Link to={"/dashboard/points"}>
+                  <DashboardCard
+                    title="Points & Badges"
+                    icon={<PiShieldCheckFill />}
+                  />
+                </Link>
+                <DashboardCard
+                  title="News & Updates"
+                  icon={<PiArticleFill />}
+                />
+                <Link to={"/dashboard/health-summary"}>
+                  <DashboardCard
+                    title="Health Summary"
+                    icon={<PiCrownSimpleBold />}
+                  />
+                </Link>
+                <Link to={"/dashboard/leader-board"}>
+                  <DashboardCard title="Leaderboard" icon={<FaStethoscope />} />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-//             MySwal.fire({
-//               icon: "info",
-//               title: "Profile Update Required",
-//               text: "hi, in other for us to know you better and personalize your experience, you are expected to update your profile",
-//               footer: '<a href="/update-profile">Update Now</a>',
-//             });
-//             // location.search
-//             loggedInUser = response.data;
-//             localStorage.setItem("user", JSON.stringify(response.data));
-//             if (!loggedInUser.accountUpdated) {
-//               navigate("/update-profile", { state: { user: response.data } });
-//             }
-//           } catch (error) {
-//             console.log(error);
-//           }
-//         };
-//         fetchgoogleUser();
-//       }, []);
-//     } else if (!loggedInUser) {
-//       useEffect(() => {
-//         MySwal.fire("you dont have access to view this page");
-//         navigate("/login");
-//       }, []);
-//     }
-//   }
-//   return (
-//     <div className="flex flex-row h-screen w-full">
-//       <SideBar isOpen={isOpen} />
-//       <div className="lgss:w-4/5 w-full lgss:overflow-auto">
-//         <div className="lgss:w-full lgss:h-full lgss:flex-1 lgss:overflow lgss:px-[4%] flex flex-col ">
-//           {loggedInUser && (
-//             <Header
-//               setIsOpen={setIsOpen}
-//               isOpen={isOpen}
-//               currentUser={loggedInUser}
-//             />
-//           )}{" "}
-//           <div className="mt-[50px] lgss:mt-0 text-[34px] text-gold font-semibold px-[5%] mds:px-0">
-//             <h1>Dashboard</h1>
-//           </div>
-//           {loggedInUser && <DetailsCard loggedInUser={loggedInUser} />}
-//           <div className="">
-//             <div className="flex flex-col justify-center lgss:items-start lgss:pt-14 pt-4">
-//               <div className=" flex flex-col lgss:grid lgss:grid-cols-3 gap-8">
-//                 <Link to={"/dashboard/profile"}>
-//                   <DashboardCard
-//                     title="Manage Profile"
-//                     icon={<PiUserCircleFill />}
-//                   />
-//                 </Link>
-//                 <Link to={"/dashboard/points"}>
-//                   <DashboardCard
-//                     title="Points & Badges"
-//                     icon={<PiShieldCheckFill />}
-//                   />
-//                 </Link>
-//                 <DashboardCard
-//                   title="News & Updates"
-//                   icon={<PiArticleFill />}
-//                 />
-//                 <Link to={"/dashboard/health-summary"}>
-//                   <DashboardCard
-//                     title="Health Summary"
-//                     icon={<PiCrownSimpleBold />}
-//                   />
-//                 </Link>
-//                 <Link to={"/dashboard/leader-board"}>
-//                   <DashboardCard title="Leaderboard" icon={<FaStethoscope />} />
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default DashboardPage;
+export default DashboardPage;
